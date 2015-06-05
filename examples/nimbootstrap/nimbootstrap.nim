4c3f229ffe58ec9ae5da77163fe5677073336571
Anatoly Galiulin <galiulin.anatoly@gmail.com>
2015-06-05 12:27:04 +0600

    Updated nimbootstrap

--- examples/nimbootstrap/nimbootstrap.nim
+++ examples/nimbootstrap/nimbootstrap.nim
@@ -1,18 +1,66 @@
 #!/usr/bin/env nimrunner
 import nimshell, os
 
-if paramCount() != 1: quit "Usage: nimboostrap PATH"
+let usageHelp = """
+Nim bootstrap script
+
+Usage:
+  nimboostrap <NIM_DIR> [<NIMBLE_DIR>]
+
+Options:
+  <NIM_DIR>       Nim installation path
+  <NIMBLE_DIR>    Nimble installation path
+"""
+
+if(paramCount() < 1 or paramCount() > 3):
+  quit usageHelp
+
 if not ?(which "git"): quit "Git must be installed!"
 
-let DIR = paramStr(1)
+let NIM_DIR = paramStr(1)
+let NIMBLE_DIR = if paramCount() > 1: paramStr(2) else: ""
 
-createDir DIR
-
->>! cmd"git clone -b master --depth 1 git://github.com/Araq/Nim.git $DIR"
-setCurrentDir DIR
+# First install nim
+createDir NIM_DIR
+>>! cmd"git clone -b master --depth 1 git://github.com/Araq/Nim.git $NIM_DIR"
+setCurrentDir NIM_DIR
 >>! cmd"git clone -b master --depth 1 git://github.com/nim-lang/csources"
 setCurrentDir "csources"
 >>! sh("." / "build")
 setCurrentDir ParDir
 >>! ("bin" / exe"nim" & " c koch.nim")
 >>! ("." / exe"koch" & " boot -d:release")
+
+echo "Don't forget to add " & (NIM_DIR / "bin") & " to your PATH"
+
+if NIMBLE_DIR == "":
+  quit()
+
+# Modify path to install nimble
+putEnv("PATH", getEnv("PATH") & $PathSep & (NIM_DIR / "bin"))
+  
+# Then install nimble
+createDir NIMBLE_DIR
+>>! cmd"git clone https://github.com/nim-lang/nimble.git $NIMBLE_DIR"
+setCurrentDir NIMBLE_DIR
+when defined(windows):
+  >>! cmd"nim c ${ "src" / "nimble" }"
+  moveFile "src" / exe"nimble", "src" / exe"nimble1"
+  >>! "src" / exe"nimble1" & " -y install"
+else:
+  >>! "nim c -r src/nimble -y install"
+
+# Modify path to use nimble
+putEnv("PATH", getEnv("PATH") & $PathSep & (getHomeDir() / ".nimble" / "bin"))
+
+# Install nimrunner
+let NIMRUNNER_DIR = mktemp()
+block:
+  defer: removeDir NIMRUNNER_DIR
+  >>! cmd"git clone https://github.com/vegansk/nimshell $NIMRUNNER_DIR"
+  setCurrentDir NIMRUNNER_DIR
+  >>! cmd"nimble -y install"
+  setCurrentDir NIMRUNNER_DIR / "examples" / "nimrunner"
+  >>! cmd"nimble -y install"
+
+echo "Don't forget to add " & (getHomeDir() / ".nimble" / "bin") & " to your PATH"
---
+++ examples/nimbootstrap/nimbootstrap.nimble
@@ -0,0 +1,10 @@
+[Package]
+name          = "nimbootstrap"
+version       = "0.1.0"
+author        = "Anatoly Galiulin"
+description   = "Download and install script for nim language"
+license       = "MIT"
+bin           = "nimbootstrap"
+
+[Deps]
+Requires: "nim >= 0.10.2, nimshell"
