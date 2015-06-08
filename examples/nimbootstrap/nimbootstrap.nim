85aec05289f23494f98088e2c8bccc9391b8dd6b
Anatoly Galiulin <vega@eldis.ru>
2015-06-08 18:25:45 +0700

    Migrate to nim's fork while waiting for merge of pull request 2883

--- examples/nimbootstrap/nimbootstrap.nim
+++ examples/nimbootstrap/nimbootstrap.nim
@@ -22,9 +22,9 @@ let NIMBLE_DIR = if paramCount() > 1: paramStr(2) else: ""
 
 # First install nim
 createDir NIM_DIR
->>! cmd"git clone -b master --depth 1 git://github.com/Araq/Nim.git $NIM_DIR"
+>>! cmd"git clone -b devel --depth 1 git://github.com/vegansk/Nim.git $NIM_DIR"
 setCurrentDir NIM_DIR
->>! cmd"git clone -b master --depth 1 git://github.com/nim-lang/csources"
+>>! cmd"git clone -b devel --depth 1 git://github.com/nim-lang/csources"
 setCurrentDir "csources"
 >>! sh("." / "build")
 setCurrentDir ParDir
@@ -37,26 +37,25 @@ if NIMBLE_DIR == "":
   quit()
 
 # Modify path to install nimble
-putEnv("PATH", getEnv("PATH") & $PathSep & (NIM_DIR / "bin"))
+putEnv("PATH", (NIM_DIR / "bin") & $PathSep & getEnv("PATH"))
   
 # Then install nimble
 createDir NIMBLE_DIR
->>! cmd"git clone https://github.com/nim-lang/nimble.git $NIMBLE_DIR"
 setCurrentDir NIMBLE_DIR
 when defined(windows):
-  >>! cmd"nim c ${ "src" / "nimble" }"
-  moveFile "src" / exe"nimble", "src" / exe"nimble1"
-  >>! "src" / exe"nimble1" & " -y install"
-else:
+  >>! cmd"git clone --depth 1 https://github.com/nim-lang/nimble.git $NIMBLE_DIR"
   >>! "nim c -r src/nimble -y install"
 
 # Modify path to use nimble
-putEnv("PATH", getEnv("PATH") & $PathSep & (getHomeDir() / ".nimble" / "bin"))
+putEnv("PATH", (getHomeDir() / ".nimble" / "bin") & $PathSep & getEnv("PATH"))
 
 # Install nimrunner
 let NIMRUNNER_DIR = mktemp()
 block:
-  defer: removeDir NIMRUNNER_DIR
+  defer:
+    try:
+      removeDir NIMRUNNER_DIR
+    except: discard
   >>! cmd"git clone https://github.com/vegansk/nimshell $NIMRUNNER_DIR"
   setCurrentDir NIMRUNNER_DIR
   >>! cmd"nimble -y install"
